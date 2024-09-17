c8f85d73e3b573fd2a6b5a36c45c2abb6ff5de08
OrlandoEduardo101 <orlando.eduardo101@gmail.com>
2024-09-17 11:05:59 -0300

    add locale logs

--- flutter_protobuf_poc/lib/main_backend.dart
+++ flutter_protobuf_poc/lib/main_backend.dart
@@ -40,6 +40,8 @@ Future<void> main() async {
       _handleProtobufResponse(request);
     } else if (request.uri.path == '/json-gzip') {
       _handleJsonGzipResponse(request); // Novo endpoint para resposta com Gzip
+    } else if (request.uri.path == '/send_location') {
+      _handleLocationPost(request);
     } else {
       request.response
         ..statusCode = HttpStatus.notFound
@@ -86,6 +88,35 @@ void _handleJsonGzipResponse(HttpRequest request) {
     ..close();
 }
 
+void _handleLocationPost(HttpRequest request) async {
+  // 1. Ler o corpo da requisição
+  var requestBody = await utf8.decodeStream(request);
+  Map<String, dynamic> data = jsonDecode(requestBody);
+
+  // 2. Extrair os dados
+  double? latitude = double.tryParse(data['latitude'].toString());
+  double? longitude = double.tryParse(data['longitude'].toString());
+  DateTime? timestamp = DateTime.tryParse(data['timestamp']);
+  addLogToFile(data);
+
+  // 3. Validar os dados (implementar validações mais robustas)
+  if (latitude == null || longitude == null) {
+    // Retornar um erro 400 (Bad Request)
+    request.response
+      ..statusCode = HttpStatus.badRequest
+      ..close();
+    return;
+  }
+
+  // 4. Processar os dados (exemplo: salvar em um banco de dados)
+  print('Latitude: $latitude, Longitude: $longitude, Timestamp: $timestamp');
+
+  // 5. Retornar uma resposta (exemplo: 200 OK)
+  request.response
+    ..statusCode = HttpStatus.ok
+    ..close();
+}
+
 List<int> compressGzip() {
   // 1. Serializa o JSON em uma String
   final jsonString = jsonEncode(responseMap);
@@ -97,4 +128,43 @@ List<int> compressGzip() {
   final gzipBytes = GZipEncoder().encode(jsonBytes)!;
 
   return gzipBytes;
+}
+
+Future<void> addLogToFile(Map<String, dynamic> novoRegistro) async {
+  try {
+    // Obtém o caminho absoluto do arquivo atual (main.dart)
+    // Constrói o caminho para um arquivo ao lado de main.dart
+
+    String path = '';
+
+    final directoryRootPath = Directory.current.path;
+
+    var pathRoot = Platform.isWindows ? '$directoryRootPath\\logs' : '$directoryRootPath/logs';
+    final directoryLog = Directory(pathRoot);
+
+    if (!directoryLog.existsSync()) {
+      directoryLog.createSync();
+    }
+
+    path = Platform.isWindows ? '${directoryLog.path}\\logs_locale.json' : '${directoryLog.path}/logs_locale.json';
+
+    // Verifica se o arquivo existe
+    bool arquivoExiste = await File(path).exists();
+
+    // Lê o arquivo existente ou cria um novo com uma lista vazia
+    List<Map<String, dynamic>> dados = [];
+    if (arquivoExiste) {
+      String conteudoJson = await File(path).readAsString();
+      dados = List<Map<String, dynamic>>.from(jsonDecode(conteudoJson));
+    }
+
+    // Adiciona o novo registro à lista
+    dados.add(novoRegistro);
+
+    // Converte a lista de volta para JSON e sobrescreve o arquivo
+    String novoJson = jsonEncode(dados); // Adiciona indentação para melhor legibilidade
+    await File(path).writeAsString(novoJson);
+  } catch (e) {
+    print('Erro ao adicionar registro: $e');
+  }
 }
